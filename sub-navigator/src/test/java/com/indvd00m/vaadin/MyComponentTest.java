1062c9ba9a7f0fc682ed1b28a7fac29d45fb04ed
indvd00m <gotoindvdum@gmail.com>
2015-11-15 22:18:09 +0300

    Generate project by maven

---
+++ sub-navigator/src/test/java/com/indvd00m/vaadin/MyComponentTest.java
@@ -0,0 +1,13 @@
+package com.indvd00m.vaadin;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class MyComponentTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
